b8b4dbafb7c388501ea724bdf602f2acb09f5249
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-10-02 23:23:00 -0300

    bug: Path images

--- dist/musics.js
+++ dist/musics.js
@@ -2,43 +2,43 @@ export const musics = [
     {
         nameMusic: "Coração de Criança",
         nameAuthor: "Dragon Ball GT",
-        pathImage: "./images/music-images/dragon-ball-gt.jpg",
+        pathImage: "./assets/music-images/dragon-ball-gt.jpg",
         path: "./assets/music/dragon-ball-gt-abertura.mp3"
     },
     {
         nameMusic: "Blue Bird",
         nameAuthor: "Naruto Shippuden",
-        pathImage: "./images/music-images/naruto-shippuden-blue-bird.jpg",
+        pathImage: "./assets/music-images/naruto-shippuden-blue-bird.jpg",
         path: "./assets/music/naruto-shippuden-blue-bird.mp3"
     },
     {
         nameMusic: "Pokémon",
         nameAuthor: "Pokémon",
-        pathImage: "./images/music-images/pokemon.jpg",
+        pathImage: "./assets/music-images/pokemon.jpg",
         path: "./assets/music/pokemon-abertura.mp3"
     },
     {
         nameMusic: "We are!",
         nameAuthor: "One Piece",
-        pathImage: "./images/music-images/one-piece.jpg",
+        pathImage: "./assets/music-images/one-piece.jpg",
         path: "./assets/music/one-piece-we-are.mp3"
     },
     {
         nameMusic: "Shinzou wo Sasageyo",
         nameAuthor: "Shingeki no Kyojin",
-        pathImage: "./images/music-images/attack-on-titan.jpg",
+        pathImage: "./assets/music-images/attack-on-titan.jpg",
         path: "./assets/music/attack-on-titan-2.mp3"
     },
     {
         nameMusic: "Bink's Sake",
         nameAuthor: "Brook",
-        pathImage: "./images/music-images/brook.jpg",
+        pathImage: "./assets/music-images/brook.jpg",
         path: "./assets/music/binks-sake.mp3"
     },
     {
         nameMusic: "Dragon Ball Z",
         nameAuthor: "Dragon Ball Z",
-        pathImage: "./images/music-images/dragon-ball-z-1.jpg",
+        pathImage: "./assets/music-images/dragon-ball-z-1.jpg",
         path: "./assets/music/dragon-ball-z-abertura-1.mp3"
     }
 ];
